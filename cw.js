df605ea71c16c1543cbaf48c8d636284e3e3df24
mciannello <87342831+mciannello@users.noreply.github.com>
2022-08-29 17:43:18 -0300

    cambio datos

--- cw.js
+++ cw.js
@@ -2,7 +2,7 @@ let identificacion = {
     nombre:"María Cecilia",
     apellido:"Iannello",
     edad:"34",
-    desc:"<ul class='apti'><li>Diseñadora Multimedial</li><li>Docente en TICs</li><li>Desarrolladora Web</li><li>FullStack Developer Python</li>"
+    desc:"<ul class='apti'><li>Diseñadora Multimedial</li><li>Docente en TICs</li><li>Desarrolladora Web</li><li>Full Stack Python</li>"
 }
 
 document.getElementById("nombre").innerHTML = identificacion.nombre+ " "+identificacion.apellido ;
