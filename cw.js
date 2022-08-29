c3ae3cc53a98f7ba639b0e6aa042c054194db604
unknown <mciannello@gmail.com>
2022-08-29 16:28:28 -0300

    primero

---
+++ cw.js
@@ -0,0 +1,9 @@
+let identificacion = {
+    nombre:"María Cecilia",
+    apellido:"Iannello",
+    edad:"34",
+    desc:"<ul class='apti'><li>Diseñadora Multimedial</li><li>Docente en TICs</li><li>Desarrolladora Web</li><li>FullStack Developer Python</li>"
+}
+
+document.getElementById("nombre").innerHTML = identificacion.nombre+ " "+identificacion.apellido ;
+document.getElementById("info").innerHTML =identificacion.desc
